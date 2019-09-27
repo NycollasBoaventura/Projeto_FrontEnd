ccb1a0e45e93edd8eaf18fa6beb9cde57aff557a
Nick maia <nickmaia1208@gmail.com>
2019-09-27 14:37:03 -0300

    Array de produtos na pagina Painel

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,27 @@
 <?php
 
+    use GuzzleHttp\Client;
+    
+    Route::get('/Painel', function () {
+    
+        $client = new Client([
+    
+            'base_uri' => 'http://192.168.0.39:8080/produtos/',
+            'timeout'  => 20,0,
+    
+        ]);
 
-    Route::get('/', function () {
-        return view('welcome');
+        $response = $client->request('GET', 'lista');
+        
+       return json_decode($response->getBody()->getContents());
+        
+        return view('lista.index', compact());
     });
+
+
 
     Auth::routes();
 
     Route::get('/', 'Home\HomeController@index')->name('home.index');
-
-
-    Route::get('/Painel', 'Painel\PainelController@index')->name('painel.index');
     Route::get('/Painel/Clientes', 'Painel\PainelController@viewClientes')->name('painel.Clientes.index');
-    
+
